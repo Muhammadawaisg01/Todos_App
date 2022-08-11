9cb65477a297bd1ca5b26a56871533a56e7e2858
Muhammad Awais <muhammadawaisg01@gmail.com>
2022-08-11 16:56:19 +0500

    working on states and props

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
 
-const element = <h1>Hello from Create React App</h1>
+const element = <h1>Hello from Create React App element</h1>
 
-ReactDOM.render(< TodoContainer / >, document.getElementById("root"));
+// ReactDOM.render(element, document.getElementById("root"));
 
+ReactDOM.render(
+<React.StrictMode>
+< TodoContainer  />
+</React.StrictMode>
+, document.getElementById("root"));
 
 
