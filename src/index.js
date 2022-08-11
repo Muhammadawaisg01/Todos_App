0daba67adaccb571462600022e4bec9928bbe207
Muhammad Awais <muhammadawaisg01@gmail.com>
2022-08-11 13:20:07 +0500

    fourth commit, all components file created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import TodoContainer from "./components/TodoContainer"
 
 const element = <h1>Hello from Create React App</h1>
 
-ReactDOM.render(element, document.getElementById("root"));
-console.log("HELLO WORLD") ; 
+ReactDOM.render(< TodoContainer / >, document.getElementById("root"));
+
+
 
