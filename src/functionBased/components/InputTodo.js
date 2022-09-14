f5566e0aa031047f1e2e892248633f404fbb8186
Muhammad Awais <muhammadawaisg01@gmail.com>
2022-09-14 18:07:38 +0500

    updated

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,42 +1,43 @@
-import React, { useState } from "react" 
-import {FaPlusCircle} from "react-icons/fa"
+import React, { useState } from "react"
+import { FaPlusCircle } from "react-icons/fa"
+import "../App.css"
 
-const InputTodo = (props) => { 
-    const [title, setTitle] = useState("") ; 
+const InputTodo = (props) => {
+    const [title, setTitle] = useState("");
     // const [inputText, setinputText] = useState( { 
     //     fname:"" , 
     //     lname:"" 
     // } ) 
-    
-    const onChange = e => { 
-        setTitle (prevTitle => prevTitle = e.target.value ) 
+
+    const onChange = e => {
+        setTitle(prevTitle => prevTitle = e.target.value)
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault() ; 
-        if(title.trim()){
-            props.addTodoProps(title) 
-            setTitle("") ; 
+        e.preventDefault();
+        if (title.trim()) {
+            props.addTodoProps(title)
+            setTitle("");
         } else {
-            alert("Enter Item correctly") ; 
+            alert("Enter Item correctly");
         }
     }
 
-    return ( 
-    <form onSubmit = {handleSubmit} className = "form-container"> 
-        <input 
-        type="text"
-        className = "input-text" 
-        value={title}
-        name = "title" 
-        placeholder="...TodoInputName"
-        onChange={onChange}
-        />
-        {/* <div onDoubleClick = {this.handleEditing} ></div>   */}
-        <br/>
-        <button className="input-submit"> <FaPlusCircle/> </button> 
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <input
+                type="text"
+                className="input-text"
+                value={title} // State for making it a controlled input 
+                name="title"
+                placeholder="Enter text..."
+                onChange={onChange}
+            />
+            {/* <div onDoubleClick = {this.handleEditing} ></div>   */}
+            <br />
+            <button className="input-submit"> <FaPlusCircle /> </button>
+        </form>
     )
-  }
+}
 
 export default InputTodo
